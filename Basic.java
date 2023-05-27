b1f9b565f5192e59bd19d293d2d124f64fbd9ca7
Safal Rana <123141574+SafalRana01@users.noreply.github.com>
2023-05-27 23:48:35 +0545

    Update Basic.java

--- Basic.java
+++ Basic.java
@@ -30,3 +30,76 @@ public class UsingArthematicOperator
                 System.out.println(mod); // OUTPUT = 0
        }
 }       
+
+
+
+3. Full Introduction
+
+public class MyFullIntroduction
+{  
+    public static void main(String[] args)
+    {
+        //Filling My Details 
+        String name="Safal Rana.";
+        String pernament_address="Syangja";
+        String temporary_address="Pokhara.";
+        String age="23 years old." ;
+        String citizenshipNo="42-02-73-03776.";
+        boolean married=false;
+        String height="164 cm.";
+        char gender = 'M'
+        String slc="BETHANY HIGHER SECONDARY SCHOOL";
+        String plus2="SHISHU NIKETAN HIGHER SECONDARY SCHOOL.";
+        String recently="INFORMATICS.";
+        String familyMembers="5 family members.Mom,Dad and my two elder sisters.";
+        String fatherName="Himal Rana.";
+        String motherName="Hima Rana.";
+        String elderSisterName="Sunita Rana";
+        String secondSisterName="Sabina Rana.";
+        String me="And I am the youngest one of my family";
+        
+        
+              //Trying to printout all in paragraph
+              System.out.println("Hello Everyone");
+              System.out.print("My Name is "+name);
+              System.out.print("I am "+age);
+              System.out.print("My Pernament Address is "+pernament_address);
+              System.out.println(" And now I live in "+temporary_address);
+              System.out.print("My Citizenship No is "+citizenshipNo);
+              System.out.print("My Height is "+height);
+        
+              if (married){
+                   System.out.print("I am Married");
+              }
+               else{
+                   System.out.print("I am not married.");
+              }
+        
+              if(gender == 'M'){
+                  System.out.print("My gender is Male");
+              }
+               else if(gender == 'F'){
+                  System.out.print("I am Female");
+              } 
+               else{
+                  System.out.printl("I am neither Male nor Female");
+              }
+        
+              System.out.print("I had completed my SLC from "+slc);
+              System.out.println(" And my +2 from "+plus2);
+              System.out.print("Recently I am studying in "+recently);
+              System.out.println("I have "+familyMembers);
+              System.out.print("My Father name is "+fatherName);
+              System.out.print("My Mother name is "+motherName);
+              System.out.println("My First sister name is "+elderSisterName);
+              System.out.print("And my second sister name is "+secondSisterName);
+              System.out.print(me);
+         
+        
+              /*
+               * THis is my full introduction in paragraph if you want to get some knowlege
+               * you can take a tips hahah 
+               */
+             }
+     }
+}
