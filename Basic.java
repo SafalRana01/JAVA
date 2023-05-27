89c622bc0f08f265a3b00e0a9244670052d8e615
Safal Rana <123141574+SafalRana01@users.noreply.github.com>
2023-05-27 23:44:20 +0545

    Update Basic.java

--- Basic.java
+++ Basic.java
@@ -1,1 +1,8 @@
+Command to write your first name with proper syntax
 
+public class MyName{
+      public static void main(String[] args)
+      {
+         System.out.println("Safal Rana"); //IT WILL PRINT MY NAME AS OUTPUT
+      }
+}   
