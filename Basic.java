da0dae0b1b3256708822d09a6efc68977685753c
Safal Rana <123141574+SafalRana01@users.noreply.github.com>
2023-06-05 19:23:39 +0545

    Update Basic.java

--- Basic.java
+++ Basic.java
@@ -293,3 +293,18 @@ public class PractiseIfElseIfClause
 
 
 9. Operator Work
+
+public class OperatorWork
+{
+     public static void main(String[] args)
+     {
+           int number = 500;
+     
+                   System.out.println(number == 500); //To compare  //OUTPUT = true
+                   System.out.println(number != 500); // Not equals to  //OUTPUT = FALSE
+                   System.out.println(number < 500); //number is less than 500 //OUTPUT = FALSE
+                   System.out.println(number > 500); // number is Greater tha 500 //OUTPUT = FALSE
+                   System.out.println(number <= 500); // number is less than or equals to 500 //OUTPUT = TRUE
+                   System.out.println(number >= 500); // number is greater than or equals to 500 //OUTPUT = TRUE
+     } 
+}
