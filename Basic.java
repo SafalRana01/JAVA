a8ea74b72ad1066c191d1b65b5dab7b9cdbb8984
Safal Rana <123141574+SafalRana01@users.noreply.github.com>
2023-06-15 20:54:04 +0545

    Update Basic.java

--- Basic.java
+++ Basic.java
@@ -3,7 +3,7 @@
 public class MyName{
       public static void main(String[] args)
       {
-         System.out.println("Safal Rana"); //IT WILL PRINT MY NAME AS OUTPUT
+         System.out.println("Safal Rana") //IT WILL PRINT MY NAME AS OUTPUT
       }
 }   
 
