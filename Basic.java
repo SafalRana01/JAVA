d6e8a629657a943afb0c08a203027a522e461743
Safal Rana <123141574+SafalRana01@users.noreply.github.com>
2023-09-06 08:21:30 +0545

    Update Basic.java

--- Basic.java
+++ Basic.java
@@ -1,3 +1,13 @@
+1.  Command to write your first name with proper syntax.
+
+public class MyName{
+      public static void main(String[] args)
+      {
+         System.out.println("Safal Rana"); //IT WILL PRINT MY NAME AS OUTPUT
+      }
+}   
+
+
 
 2.  Using of Arthematics Operator
 
